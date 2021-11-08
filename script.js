6699b0f3c7b190680da0c77386e4583b4e1f5dbe
joneskj55 <joneskj55@gmail.com>
2021-11-08 14:50:55 -0600

    add reset function and comments

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
+// declare score variable
 let score = 0;
 
+// free throw
 function onePoint() {
   score += 1;
   return score;
 }
 
+// two-pointer
 function twoPoints() {
   score += 2;
   return score;
 }
 
+// three-pointer
 function threePoints() {
   score += 3;
   return score;
 }
 
-console.log(onePoint() + twoPoints() + threePoints());
+// reset game
+function reset() {
+  score = 0;
+  return score;
+}
+
+console.log(reset());
