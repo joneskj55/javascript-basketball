859981e233fc47fc7398f5d04ab331a6b5a74119
joneskj55 <joneskj55@gmail.com>
2021-11-08 14:28:34 -0600

    add initial logic for scoring

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-// increment score
-function score() {
-  var score = 0;
-  // switch statement to implement score by 1, 2, or 3
-  switch (score) {
-    case 1:
-      score = score + 1;
-      break;
-    case 2:
-      score = score + 2;
-      break;
-    case 3:
-      score = score + 3;
-      break;
-    default:
-      score = score + 0;
-  }
-  // increment score by 1
-  score++;
-  document.getElementById("score").innerHTML = score;
-  // increment score by 2
-  score += 2;
-  // increment score by 3
-  score += 3;
+// declare score variable
+let score = 0;
+
+// declare function to add one point to score
+function onePoint() {
+  score = score += 1;
+  return score;
 }
+
+function twoPoints() {
+  score += 2;
+  return score;
+}
+
+function threePoints() {
+  score += 3;
+  return score;
+}
+
+console.log(onePoint() + twoPoints() + threePoints());
