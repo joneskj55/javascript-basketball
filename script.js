56497dbc7b8507b865a027b54e3c073503d8f372
joneskj55 <joneskj55@gmail.com>
2021-11-09 14:13:58 -0600

    initial functionality complete

--- script.js
+++ script.js
@@ -1,30 +1,57 @@
-// declare score variable
-let score = 0;
+// declare score variables
+let score1 = 0;
+let score2 = 0;
+
+// player 1
 
 // free throw
-function onePoint() {
-  score += 1;
-  console.log(score);
-  return score;
+function onePoint1() {
+  score1 += 1;
+  document.getElementById("player-one-score").innerHTML = score1;
+  return score1;
 }
 
 // two-pointer
-function twoPoints() {
-  score += 2;
-  console.log(score);
-  return score;
+function twoPoints1() {
+  score1 += 2;
+  document.getElementById("player-one-score").innerHTML = score1;
+  return score1;
 }
 
 // three-pointer
-function threePoints() {
-  score += 3;
-  console.log(score);
-  return score;
+function threePoints1() {
+  score1 += 3;
+  document.getElementById("player-one-score").innerHTML = score1;
+  return score1;
+}
+
+// player 2
+
+// free throw
+function onePoint2() {
+  score2 += 1;
+  document.getElementById("player-two-score").innerHTML = score2;
+  return score2;
+}
+
+// two-pointer
+function twoPoints2() {
+  score2 += 2;
+  document.getElementById("player-two-score").innerHTML = score2;
+  return score2;
+}
+
+// three-pointer
+function threePoints2() {
+  score2 += 3;
+  document.getElementById("player-two-score").innerHTML = score2;
+  return score2;
 }
 
 // reset game
 function reset() {
-  score = 0;
-  console.log(score);
-  return score;
+  score1 = 0;
+  score2 = 0;
+  document.getElementById("player-one-score").innerHTML = score1;
+  document.getElementById("player-two-score").innerHTML = score2;
 }
