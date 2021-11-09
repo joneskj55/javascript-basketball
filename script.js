232644fab4e7381015bbe3ef13cd63dac10d7cfb
joneskj55 <joneskj55@gmail.com>
2021-11-09 14:01:31 -0600

    connect buttons to functions

--- script.js
+++ script.js
@@ -4,25 +4,27 @@ let score = 0;
 // free throw
 function onePoint() {
   score += 1;
+  console.log(score);
   return score;
 }
 
 // two-pointer
 function twoPoints() {
   score += 2;
+  console.log(score);
   return score;
 }
 
 // three-pointer
 function threePoints() {
   score += 3;
+  console.log(score);
   return score;
 }
 
 // reset game
 function reset() {
   score = 0;
+  console.log(score);
   return score;
 }
-
-console.log(reset());
