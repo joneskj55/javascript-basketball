6e368730374ec32544d55a2806c7842681c0690e
joneskj55 <joneskj55@gmail.com>
2021-11-12 14:43:21 -0600

    add initial shotclock countdown

--- script.js
+++ script.js
@@ -42,6 +42,32 @@ function threePoints2() {
   document.getElementById("player-two-score").innerHTML = score2;
 }
 
+// shot clock
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculation for seconds
+  var seconds = Math.floor((distance % (1000 * 25)) / 1000);
+
+  // Output the result in an element with id="clock"
+  document.getElementById("clock").innerHTML = seconds;
+
+  // If the count down is over, write some text
+  // if (distance < 0) {
+  //   clearInterval(x);
+  //   document.getElementById("clock").innerHTML = "EXPIRED";
+  //   // BUZZER
+  // }
+}, 1000);
+
 // reset game
 function reset() {
   score1 = 0;
