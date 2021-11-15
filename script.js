d9fd956deefe1a7a0ca774d99aaa06f2c57ff6e5
joneskj55 <joneskj55@gmail.com>
2021-11-15 09:49:56 -0600

    clean up js

--- script.js
+++ script.js
@@ -44,21 +44,19 @@ function threePoints2() {
 
 // shot clock
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+const countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function () {
+const x = setInterval(function () {
   // Get today's date and time
-  var now = new Date().getTime();
+  const now = new Date().getTime();
 
   // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+  const distance = countDownDate - now;
 
   // Time calculation for seconds
-  var seconds = Math.floor((distance % (1000 * 25)) / 1000);
-
   // Output the result in an element with id="clock"
-  document.getElementById("clock").innerHTML = seconds;
+  document.getElementById("clock").innerHTML = Math.floor((distance % (1000 * 25)) / 1000);
 
   // If the count down is over, write some text
   // if (distance < 0) {
